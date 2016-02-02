2de193391871fcefc2cc451f16b8a94bf34d0683
devincody <devincody@gmail.com>
2016-02-02 02:10:24 -0500

    added interpolate_outliers() function to smooth outliers from data sets

--- wg_simulator.py
+++ wg_simulator.py
@@ -19,6 +19,10 @@ class simulated_wg(object):
         self.inductance = np.load(name)
         name = prefix + "current" + ".npy"
         self.current = np.load(name)
+        name = "../data/parametersangles.npy"
+        self.angles = np.load(name)
+        name = "../data/parameterseigenmodes.npy"
+        self.eigenmodes = np.load(name)
     
     def build_L_mat(self):
         pass
@@ -26,21 +30,40 @@ class simulated_wg(object):
     def build_C_mat(self):
         pass
 
-def interpolate_outliers(angle, data, m=2):
+def interpolate_outliers(angle, data, m=1.5,mv_av = 8):
+    '''
+    Function to smooth outliers from the data set. Applys moving
+    average smoothing and cyclic boundary conditions. Threshold
+    is set by:
+    m - number of standard deviations from average which defines outliers
+    mv_av - number of points in each direction used for average
+    '''
     data2 = []
     mean = np.mean(data)
     std = np.std(data)
-    f = interp1d(angle, data)
     for i in range(len(data)):
         if abs(data[i] - mean) < m*std:
             data2.append(data[i])
         else:
-            data2.append(int(f(angle[i])))
+            ang_last = len(angle)
+            if (i-mv_av < 0):
+                comb = np.concatenate((data[0: i+8],data[ang_last + i - mv_av: ang_last]))
+                print comb
+                data2.append(np.mean(comb))
+            elif(i+mv_av > ang_last):
+                comb = np.concatenate((data[i-8: ang_last], data[0:i+mv_av - ang_last]))
+                print comb
+                data2.append(np.mean(comb))
+            else:
+                data2.append(np.mean(data[i-8:i+8]))
     return data2
 
 
 def main():
-    wg = simulated_wg()    
+    wg = simulated_wg()
+    voltage = interpolate_outliers(wg.angles,wg.inductance)
+    plt.plot(wg.angles,voltage)
+    plt.show()
 
     
 if __name__ == "__main__":

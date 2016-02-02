85e4266b6d1ae4d31817393cb1ffe4a5191271d4
devincody <devincody@gmail.com>
2016-02-02 12:02:56 -0500

    added code to build L and C matricies

--- wg_simulator.py
+++ wg_simulator.py
@@ -25,12 +25,25 @@ class simulated_wg(object):
         self.eigenmodes = np.load(name)
     
     def build_L_mat(self):
-        pass
+        size = len(self.inductance)
+        self.L = np.zeros((size,size))
+        for i in range(size):
+            self.L[i][i] += self.inductance[i]**-1
+            self.L[(i+1)%size][i] += -self.inductance[i]**-1
+            self.L[i][(i+1)%size] += -self.inductance[i]**-1
+            self.L[(i+1)%size][(i+1)%size] += self.inductance[i]**-1
+        print self.L
 
     def build_C_mat(self):
         pass
 
-def interpolate_outliers(angle, data, m=1.5,mv_av = 8):
+    def test_interpolate(self):
+        potted = self.inductance
+        voltage = interpolate_outliers(self.angles,potted)
+        plt.plot(self.angles,voltage, self.angles, potted)
+        plt.show()
+
+def interpolate_outliers(angle, data, m=.5,mv_av = 8):
     '''
     Function to smooth outliers from the data set. Applys moving
     average smoothing and cyclic boundary conditions. Threshold
@@ -48,23 +61,20 @@ def interpolate_outliers(angle, data, m=1.5,mv_av = 8):
             ang_last = len(angle)
             if (i-mv_av < 0):
                 comb = np.concatenate((data[0: i+8],data[ang_last + i - mv_av: ang_last]))
-                print comb
                 data2.append(np.mean(comb))
             elif(i+mv_av > ang_last):
                 comb = np.concatenate((data[i-8: ang_last], data[0:i+mv_av - ang_last]))
-                print comb
                 data2.append(np.mean(comb))
             else:
                 data2.append(np.mean(data[i-8:i+8]))
     return data2
 
 
+        
 def main():
-    wg = simulated_wg()
-    voltage = interpolate_outliers(wg.angles,wg.inductance)
-    plt.plot(wg.angles,voltage)
-    plt.show()
-
+    sim_wg = simulated_wg()
+    sim_wg.test_interpolate()
+    sim_wg.build_L_mat()
     
 if __name__ == "__main__":
     main()            

95ec39d61f673dbf47487e501d00b50e4e8a88a3
devincody <devincody@gmail.com>
2016-02-03 12:38:53 -0500

    used rolling average to smooth data, frequencies within an oom

--- wg_simulator.py
+++ wg_simulator.py
@@ -28,10 +28,11 @@ class simulated_wg(object):
         self.eigenmodes = np.load(name)
     
     def build_L_mat(self):
-        smooth_l = interpolate_outliers(self.angles, self.inductance)        
+        smooth_l = interpolate_outliers(self.angles, self.inductance)   
+        smooth_l = interpolate_outliers(self.angles, smooth_l) 
         size = len(smooth_l)
         self.L = np.zeros((size,size))
-        d_l = 1#5.89*.001*2*np.pi/100
+        d_l = 5.89*.001*2*np.pi/100
         for i in range(size):
             self.L[i][i] += (d_l*smooth_l[i])**-1
             self.L[(i+1)%size][i] += -(d_l*smooth_l[i])**-1
@@ -40,9 +41,10 @@ class simulated_wg(object):
 
     def build_C_mat(self):
         smooth_c = interpolate_outliers(self.angles, self.capacitance)
+        smooth_c = interpolate_outliers(self.angles, smooth_c)
         size = len(smooth_c)
         self.C = np.zeros((size,size))
-        d_l = 1#5.89*.001*2*np.pi/100
+        d_l = 5.89*.001*2*np.pi/100
         for i in range(size):
             self.C[i][i] += (d_l*smooth_c[i])
             self.C[(i+1)%size][i] += -(d_l*smooth_c[i])
@@ -54,14 +56,15 @@ class simulated_wg(object):
         voltage = interpolate_outliers(self.angles,potted, plot_me = plot_me)
         plt.plot(self.angles,voltage, self.angles, potted)
         ax = plt.gca()
-        ax.set_ylim(min(voltage), max(voltage))
+        ax.set_ylim(min(voltage)/1.05, max(voltage)*1.05)
         plt.show()
         
     def get_frequencies(self):
         LC = np.dot(np.linalg.inv(self.C),self.L)
-        print LC
-        self.evals = np.linalg.eigvals(LC)
-        print np.sqrt(self.evals)
+        #print LC
+        self.evals = -np.linalg.eigvals(LC)
+        self.calc_freq = np.sqrt(self.evals)/(2*np.pi)
+        print np.sort(self.calc_freq)
 
 def interpolate_outliers(angle, data, threshold=1., window = 5, plot_me = False):
     '''
@@ -76,18 +79,28 @@ def interpolate_outliers(angle, data, threshold=1., window = 5, plot_me = False)
     df['data_mean'] = pd.rolling_median(df['parameter'].copy(), window=window, center=True).fillna(method='bfill').fillna(method='ffill')
     difference = np.abs(df['parameter'] - df['data_mean'])#mean_data)#
     outlier_idx = difference > threshold*df['parameter'].std()    
-    
-    s = df['parameter'].copy()
-    s[outlier_idx] = np.nan
-    s.interpolate(method='spline', order=1, inplace=True)
+    #df['data_mean'].plot()
+#    s = df['parameter'].copy()
+#    s[outlier_idx] = np.nan
+#    s.interpolate(method='spline', order=1, inplace=True)
+#    df['cleaned_parameter'] = s
+    tst = np.array(outlier_idx)
+    datamean= np.array(df['data_mean'])
+    s = np.array(df['parameter'])
+    itms = len(outlier_idx)
+    for i in range(itms):
+        if tst[i] == True or tst[(i-1)%itms] == True or tst[(i+1)%itms] == True:
+            tmp =  datamean[i]
+            s[i] = tmp
+    #print s
     df['cleaned_parameter'] = s
     
-    if (plot_me == True):
-        figsize = (7, 2.75)
-        fig, ax = plt.subplots(figsize=figsize)
-        df['parameter'].plot()
-        df['cleaned_parameter'].plot()
-        ax.set_ylim(min(df['cleaned_parameter']), max(df['cleaned_parameter']))
+#    if (plot_me == True):
+#        figsize = (7, 2.75)
+#        fig, ax = plt.subplots(figsize=figsize)
+#        df['parameter'].plot()
+#        df['cleaned_parameter'].plot()
+#        ax.set_ylim(min(df['cleaned_parameter']), max(df['cleaned_parameter']))
     return np.array(df['cleaned_parameter'])
 
 
@@ -95,9 +108,9 @@ def interpolate_outliers(angle, data, threshold=1., window = 5, plot_me = False)
 def main():
     sim_wg = simulated_wg()
     sim_wg.test_interpolate()
-    #sim_wg.build_L_mat()
-    #sim_wg.build_C_mat()
-    #sim_wg.get_frequencies()
+    sim_wg.build_L_mat()
+    sim_wg.build_C_mat()
+    sim_wg.get_frequencies()
     
     
 if __name__ == "__main__":
